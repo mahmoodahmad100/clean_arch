18bcde8ef518b6a777eba91c9e90c880e95e61e3
mahmoodahmad100 <mahmood073@gmail.com>
2017-12-25 10:50:58 +0200

    init customer entity

---
+++ src/Domain/Entity/Customer.php
@@ -0,0 +1,34 @@
+<?php 
+namespace CleanArch\Invoicer\Domain\Entity;
+
+class Customer extends AbstractEntity
+{
+
+	protected $name;
+	protected $email;
+
+	public function getName()
+	{
+		return $this->name;
+	}
+
+	public function setName($name)
+	{
+		$this->name = $name;
+		return $this;
+	}
+
+	public function getEmail()
+	{
+		return $this->email;
+	}
+
+	public function setEmail($email)
+	{
+		$this->email = $email;
+		return $this;
+	}
+
+}
+
+ ?>
