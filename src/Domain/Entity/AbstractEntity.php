1a3e4343258c46430bf677a5e40c4206d4d4e935
mahmoodahmad100 <mahmood073@gmail.com>
2017-12-25 10:51:44 +0200

    set & get

--- src/Domain/Entity/AbstractEntity.php
+++ src/Domain/Entity/AbstractEntity.php
@@ -3,7 +3,17 @@ namespace CleanArch\Invoicer\Domain\Entity;
 
 abstract AbstractEntity
 {
-	
+	protected $id;
+
+	public function getId(){
+		return $this->id;
+	}
+
+	public function setId($id)
+	{
+		$this->id = $id;
+		return $this;
+	}
 }
 
 
