1e068b019e00d0a01c8ab731c72ed7fac17f974a
mahmoodahmad100 <mahmood073@gmail.com>
2018-01-01 21:03:33 +0200

    basic setup

--- specs/domain/service/invoice-factory.spec.php
+++ specs/domain/service/invoice-factory.spec.php
@@ -3,3 +3,18 @@
 use CleanArch\Invoicer\Domain\Entity\Invoice;
 use CleanArch\Invoicer\Domain\Entity\Order;
 use CleanArch\Invoicer\Domain\Factory\InvoiceFactory;
+
+descripe('InvoiceFactory', function(){
+	descripe('->createFromOrder()',function(){
+
+		it("should return an order object", function(){
+			$order   = new Order();
+			$factory = new InvoiceFactory();
+			$invoice = $factory->createFromOrder($order);
+
+			expect($invoice)->to->be->instanceof(
+				'CleanArch\Invoicer\Domain\Entity\Invoice'
+			);	
+		});
+	})
+});
