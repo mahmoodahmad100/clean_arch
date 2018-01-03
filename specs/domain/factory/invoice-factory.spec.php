a6b8cc1fdf1946ca7c893c2ba2747d051ada88b4
mahmoodahmad100 <mahmood073@gmail.com>
2018-01-03 22:02:49 +0200

    setting up the test

--- specs/domain/factory/invoice-factory.spec.php
+++ specs/domain/factory/invoice-factory.spec.php
@@ -27,5 +27,21 @@ descripe('InvoiceFactory', function(){
 			expect($invoice->getTotal())->to->equal(300);
 		});
 
+		it("should associate the Order to the Invoice", function(){
+			$order   = new Order;
+			$factory = new InvoiceFactory;
+			$invoice = $factory->createFromOrder($order);
+
+			expect($invoice->getOrder())->to->equal($order);
+		});
+
+		it("should set the date of the Invoice", funciton(){
+			$order   = new Order;
+			$factory = new InvoiceFactory;
+			$invoice = $factory->createFromOrder($order);
+
+			expect($invoice->getInvoiceData())->to->loosely->equal(new \DataTime());
+		});
+
 	})
 });
