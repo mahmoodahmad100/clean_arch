6053e373298659ab3927e270035bcfb8966415cb
mahmoodahmad100 <mahmood073@gmail.com>
2018-01-07 19:21:06 +0200

    basic setup

---
+++ specs/domain/service/invoicing-service.spec.php
@@ -0,0 +1,18 @@
+<?php
+
+$repo = 'CleanArch\Invoicer\Domain\Repository\OrderRepositoryInterface';
+
+describe("InvoicingService", function(){
+	describe("->generateInvoices()", function(){
+
+		beforeEach(function(){
+			$this->repository = $this->getProphet()->prophesize($repo);
+		});
+
+		it("should query the repository for uninvoiced Orders",function(){
+			$this->repository->getUninvoicedOrders()->shouldBeCalled();
+			$service = new InvoicingService($this->repository->reveal());
+			$service->generateInvoices();
+		});
+	});
+});
