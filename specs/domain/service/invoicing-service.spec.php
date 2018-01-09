5183cff9f23d0b63fd5214a477aed8129ff28436
mahmoodahmad100 <mahmood073@gmail.com>
2018-01-09 22:56:51 +0200

    setting the tests for 'InvoicingService'

--- specs/domain/service/invoicing-service.spec.php
+++ specs/domain/service/invoicing-service.spec.php
@@ -1,18 +1,47 @@
 <?php
 
-$repo = 'CleanArch\Invoicer\Domain\Repository\OrderRepositoryInterface';
+use CleanArch\Invoicer\Domain\Entity\Invoice;
+use CleanArch\Invoicer\Domain\Entity\Order;
+use CleanArch\Invoicer\Domain\Service\InvoicingService;
 
 describe("InvoicingService", function(){
 	describe("->generateInvoices()", function(){
 
 		beforeEach(function(){
-			$this->repository = $this->getProphet()->prophesize($repo);
+			$this->repository = $this->getProphet()->prophesize('CleanArch\Invoicer\Domain\Repository\OrderRepositoryInterface');
+			$this->factory = $this->getProphet()->prophesize('CleanArch\Invoicer\Domain\Factory\InvoiceFactory');
 		});
 
 		it("should query the repository for uninvoiced Orders",function(){
+
 			$this->repository->getUninvoicedOrders()->shouldBeCalled();
-			$service = new InvoicingService($this->repository->reveal());
+
+			$service = new InvoicingService(
+				$this->repository->reveal(),$this->factory->reveal(),
+				$this->factory->reveal()
+			);
+
 			$service->generateInvoices();
+		});
+
+		it("should return an Invoice for each uninvoiced Order", function(){
+
+			$orders   = [(new Order())->setTotal(300)];
+			$invoices = [(new Invoice())->setTotal(300)];
+
+			$this->repository->getUninvoicedOrders()->willReturn($orders);
+			$this->factory->createFromOrder($orders[0])->willReturn($invoices[0]);
+
+			$service = new InvoicingService(
+				$this->repository->reveal(),
+				$this->factory->reveal()
+			);
+
+			$results = $service->generateInvoices();
+
+			expect($results)->to->be->a('array');
+			expect($results)->to->have->length(count($orders));
+
 		});
 
 		afterEach(function () {
